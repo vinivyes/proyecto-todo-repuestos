aebb70b900f2e363cabafe3d4d941f72b8937d76
Vinicius de Morais <viniciusmorais352@gmail.com>
2022-06-17 19:41:36 -0600

    Initial Commit

---
+++ src/main/java/com/todorepuestos/TodoRespuestosApplication.java
@@ -0,0 +1,13 @@
+package com.todorepuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRespuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRespuestosApplication.class, args);
+	}
+
+}
