766d865aaf7418e6602186837d4d8f996254dbf7
Vinicius de Morais <viniciusmorais352@gmail.com>
2022-07-13 21:29:07 -0600

    Index Controller

---
+++ src/main/java/com/todorepuestos/controller/IndexController.java
@@ -0,0 +1,18 @@
+
+package com.todorepuestos.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class IndexController {
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        
+        return "index";
+    }
+}
